92a3bfde9e354e6d7269421518f04f0851c19042
Madison Lawton <madisonlawton@dswinc.com>
2019-06-08 15:29:09 -0400

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './app/layout/App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Define the html element to be replaced
+const rootElement = document.getElementById('root');
+
+// Define the function to replace that html element with the application
+let render = () => {
+    ReactDOM.render(<App />, rootElement);
+}
+
+// Automatically update that application while running
+if (module.hot) {
+    module.hot.accept('./app/layout/App', () => {
+        setTimeout(render);
+    })
+}
+
+// Render (duh!)
+render();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
