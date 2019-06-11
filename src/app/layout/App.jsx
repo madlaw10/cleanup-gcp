63335508a8dc0c5032569238fe559ce62920a53f
Madison Lawton <madisonlawton@dswinc.com>
2019-06-11 08:52:55 -0400

    first component

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import CleanupDashboard from '../../features/cleanup/cleanupDashboard/CleanupDashboard';
 
 class App extends Component {
   render() {
     return (
       <div>
         <h1>Cleanup</h1>
+        <CleanupDashboard />
       </div>
     );
   }
